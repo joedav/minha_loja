7608d26d0d18021ca496f26fc38c1029f8399887
gabrielwbenatti <70673950+gabrielwbenatti@users.noreply.github.com>
2021-05-26 20:41:34 -0300

    Substituição de arquivos

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,25 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Minha Loja',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Minha Loja'),
       ),
       body: Center(
-        child: Text('MInha Loja'),
+        child: Text('Vamos desenvolver uma loja?'),
       ),
     );
   }

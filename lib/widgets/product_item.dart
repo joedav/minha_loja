0132cf40f9b80077ae52d565819253ed19c10c62
gabrielwbenatti <70673950+gabrielwbenatti@users.noreply.github.com>
2021-05-26 21:48:53 -0300

    Visualização e navegação de detalhes de produto

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:minha_loja/models/product.dart';
+import 'package:minha_loja/utils/app_routes.dart';
 
 class ProductItem extends StatelessWidget {
   final Product product;
@@ -7,21 +8,39 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridTile(
-      child: Image.network(
-        product.imageUrl,
-        fit: BoxFit.cover,
-      ),
-      footer: GridTileBar(
-        backgroundColor: Colors.black54,
-        leading: IconButton(
-          icon: Icon(Icons.favorite_border),
-          onPressed: () {},
-        ),
-        title: Text(product.title),
-        trailing: IconButton(
-          icon: Icon(Icons.shopping_cart_outlined),
-          onPressed: () {},
+    return InkWell(
+      onTap: () {
+        Navigator.of(context).pushNamed(
+          AppRoutes.PRODUCT_DETAIL,
+          arguments: product,
+        );
+      },
+      borderRadius: BorderRadius.circular(10),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(10),
+        child: GridTile(
+          child: Image.network(
+            product.imageUrl,
+            fit: BoxFit.cover,
+          ),
+          footer: GridTileBar(
+            backgroundColor: Colors.black87,
+            leading: IconButton(
+              icon: Icon(
+                Icons.favorite_outline,
+                color: Theme.of(context).accentColor,
+              ),
+              onPressed: () {},
+            ),
+            title: Text(product.title),
+            trailing: IconButton(
+              icon: Icon(
+                Icons.shopping_cart_outlined,
+                color: Theme.of(context).accentColor,
+              ),
+              onPressed: () {},
+            ),
+          ),
         ),
       ),
     );

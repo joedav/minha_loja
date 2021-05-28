9f35346a4b748992b2326e5e56deed48083d321d
gabrielwbenatti <70673950+gabrielwbenatti@users.noreply.github.com>
2021-05-27 21:07:32 -0300

    Provider

--- lib/views/products_overview_screen.dart
+++ lib/views/products_overview_screen.dart
@@ -1,13 +1,42 @@
 import 'package:flutter/material.dart';
-
+import 'package:provider/provider.dart';
 import '../widgets/product_grid.dart';
+import '../providers/products.dart';
+
+enum FilterOptions {
+  Favorite,
+  All,
+}
 
 class ProducstOverviewScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final Products products = Provider.of(context);
     return Scaffold(
       appBar: AppBar(
         title: Text('Minha Loja'),
+        actions: [
+          PopupMenuButton(
+            onSelected: (FilterOptions selectedValue) {
+              if (selectedValue == FilterOptions.Favorite) {
+                products.showFavoriteOnly();
+              } else {
+                products.showAll();
+              }
+            },
+            // icon: Icon(Icons.more_vert),
+            itemBuilder: (_) => [
+              PopupMenuItem(
+                child: Text('Somente favoritos'),
+                value: FilterOptions.Favorite,
+              ),
+              PopupMenuItem(
+                child: Text('Todos'),
+                value: FilterOptions.All,
+              ),
+            ],
+          ),
+        ],
       ),
       body: ProductGrid(),
     );

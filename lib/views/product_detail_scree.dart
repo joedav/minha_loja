62af9704ddb58db39e7f8b3a34576ccfd1a05061
gabrielwbenatti <70673950+gabrielwbenatti@users.noreply.github.com>
2021-05-28 18:42:34 -0300

    Alteração de pasta

--- lib/views/product_detail_scree.dart
+++ lib/views/product_detail_scree.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:minha_loja/models/product.dart';
+import 'package:minha_loja/providers/product.dart';
 
 class ProductDetailScreen extends StatelessWidget {
   // final Product product;
